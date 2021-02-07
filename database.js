4fb33f6b8628064f4e979b124e30b973331f0e92
natishakhadgi <natisha.khadgi@sjsu.edu>
2021-02-06 23:37:11 -0800

    fixed styling

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
     apiKey: "AIzaSyBob3XdSOukVERzADCh9lslU78qVoT1s-s",
     authDomain: "femme-hacks-hub.firebaseapp.com",
     databaseURL: "https://femme-hacks-hub-default-rtdb.firebaseio.com",
@@ -9,37 +9,34 @@
     messagingSenderId: "507237756023",
     appId: "1:507237756023:web:d4daa3fe3b0bc3ebc4ea72",
     measurementId: "G-GD53MWJZZL"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-    //Retrieve Information
-    var nameForm, linkURL, imageURL, email, mentalHealthBool, entertainmentBool, groupBool, singleBool, freeBool, physicalBool, gameBool;
-    function retrieveResource(){
-        nameForm = document.getElementById("nameForm").value;
-        linkURL = document.getElementById("linkURL").value;
-        imageURL = document.getElementById("imageURL").value;
-        emailForm = document.getElementById("emailForm").value;
-        gameBool = document.getElementById("gameBool").value;
-        entertainmentBool = document.getElementById("entertainmentBool").value;
-        physicalBool = document.getElementById("physicalBool").value;
-        mentalHealthBool = document.getElementById("mentalHealthBool").value;
-        groupBool = document.getElementById("groupBool").value;
-        singleBool = document.getElementById("singleBool").value;
-        freeBool = document.getElementById("freeBool").value;
+//Form: Retrieve Information
+var nameForm, linkURL, imageURL, mentalHealthBool, entertainmentBool, groupBool, singleBool, freeBool, physicalBool, gameBool;
 
-        console.log("start works!");
-    }
+function retrieveResource() {
+    nameForm = document.getElementById("nameForm").value;
+    linkURL = document.getElementById("linkURL").value;
+    gameBool = document.getElementById("gameBool").value;
+    entertainmentBool = document.getElementById("entertainmentBool").value;
+    physicalBool = document.getElementById("physicalBool").value;
+    mentalHealthBool = document.getElementById("mentalHealthBool").value;
+    groupBool = document.getElementById("groupBool").value;
+    singleBool = document.getElementById("singleBool").value;
+    freeBool = document.getElementById("freeBool").value;
 
-    //Write Resource
-    function writeResource(){
-      retrieveResource();
-    firebase.database().ref("Resource/"+nameForm).set({
+    console.log("start works!");
+}
+
+//Write Resource
+function writeResource() {
+    retrieveResource();
+    firebase.database().ref("Resource/" + nameForm).set({
         name: nameForm,
         link: linkURL,
-        imageLink: imageURL,
-        email: emailForm,
         game: gameBool,
         entertainment: entertainmentBool,
         physical: physicalBool,
@@ -49,4 +46,27 @@
         free: freeBool
     });
     console.log("is this working");
+    readData();
 }
+
+//Database: Retrieve Information
+// function readData(){
+//   console.log("test");
+//   var listofResources = firebase.database().ref("Resource").once("value").then(
+//     (snapshot) => {
+//     window.alert(snapshot.toString());
+//   });
+// }
+
+function readData() {
+    var query = firebase.database().ref("Resource").orderByKey();
+    query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                // key will be "ada" the first time and "alan" the second time
+                var key = childSnapshot.key;
+                // childData will be the actual contents of the child
+                var childData = childSnapshot.val();
+            });
+        });
+} //end of function
